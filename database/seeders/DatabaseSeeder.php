e9eb69a2367db1cf50aa2689b6ae074074ec96fe
thanh12-dtu-test <62973821+thanh12-dtu-test@users.noreply.github.com>
2021-10-02 02:53:46 +0700

    database: change default config

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+            JobDetailSeeder::class,
+            JobKeywordSeeder::class,
+        ]);
+        // composer dump-autoload before seeder database
     }
 }
