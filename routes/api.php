b7257ca641aedc9f54f4cae2fa7769126a2ca7a1
thanh12-dtu-test <62973821+thanh12-dtu-test@users.noreply.github.com>
2021-10-09 02:59:36 +0700

    api: add some routers
    - /api/login (params: email, password)
    - /api/logout (params: api_token)
    - /api/candidate/jobs (params: api_token) => page home of candidate
    - /api/candidate/jobs/{id} (params: api_token) => page job detail of candidate
    - /api/recruiter/applies (params: api_token) => page home of recruiter
    - /api/recruiter/applies/{job_id} (params: api_token) => page job apply of recruiter

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('login', function () {
+    return response()->json([
+        'code' => 0,
+        'message' => 'Chưa đăng nhập',
+    ]);
+})->name('login');
+Route::post('login', [App\Http\Controllers\API\UserController::class, 'login']);
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('logout', [App\Http\Controllers\API\UserController::class, 'logout']);
+    Route::get('candidate/jobs', [App\Http\Controllers\API\JobDetailController::class, 'index']);
+    Route::get('candidate/jobs/{id}', [App\Http\Controllers\API\JobDetailController::class, 'show']);
+    Route::get('recruiter/applies', [App\Http\Controllers\API\JobApplyController::class, 'index']);
+    Route::get('recruiter/applies/{job_id}', [App\Http\Controllers\API\JobApplyController::class, 'show']);
 });
