2fa6932d92356f33960d6795d5829635f63f5b22
yensubldg <thang11a5tp@gmail.com>
2021-10-26 11:37:25 +0700

    complete convert pdf to text

--- app/Http/Controllers/API/ScanCV.php
+++ app/Http/Controllers/API/ScanCV.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+/**
+ * Import the PDF Parser class
+ */
+use Smalot\PdfParser\Parser;
 
 class ScanCV extends Controller
 {
@@ -12,7 +15,22 @@ class ScanCV extends Controller
     {
         $file = $request->file('file');
         $file->move('uploads', $file->getClientOriginalName());
-        $image = 'uploads/'.$file->getClientOriginalName();
-        return response()->json(['success' => $image]);
+        $pdfFilePath  = 'uploads/'.$file->getClientOriginalName();
+        $parser = new Parser();
+        $pdf = $parser->parseFile($pdfFilePath);
+        $pages  = $pdf->getPages();
+         $totalPages = count($pages);
+          $currentPage = 1;
+          $text = "";
+            foreach ($pages as $page) {
+                $text .= "<h3>Page $currentPage/$totalPages</h3> </br>";
+                $text .= $page->getText();
+                $currentPage++;
+            }
+        // $text = str_replace("\n", "</br>", $text);
+        // $text = str_replace("\r", "</br>", $text);
+        $text = str_replace("\t", "", $text);
+        $text = str_replace("  ", "", $text);
+        return response()->json(['data' => $text]);
     }
 }
