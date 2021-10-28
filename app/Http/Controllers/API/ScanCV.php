8e86cccf1afa9acb2c55504c3720fc0ac4af56bb
yensubldg <thang11a5tp@gmail.com>
2021-10-28 21:22:52 +0700

    upload scan CV

--- app/Http/Controllers/API/ScanCV.php
+++ app/Http/Controllers/API/ScanCV.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+
 /**
  * Import the PDF Parser class
  */
+
 use Smalot\PdfParser\Parser;
 
 class ScanCV extends Controller
@@ -14,19 +16,19 @@ class ScanCV extends Controller
     public function index(Request $request)
     {
         $file = $request->file('file');
-        $file->move('uploads', $file->getClientOriginalName());
-        $pdfFilePath  = 'uploads/'.$file->getClientOriginalName();
+        $file->move('uploads', time() . $file->getClientOriginalName());
+        $pdfFilePath  = 'uploads/' . time() . $file->getClientOriginalName();
         $parser = new Parser();
         $pdf = $parser->parseFile($pdfFilePath);
         $pages  = $pdf->getPages();
-         $totalPages = count($pages);
-          $currentPage = 1;
-          $text = "";
-            foreach ($pages as $page) {
-                $text .= "<h3>Page $currentPage/$totalPages</h3> </br>";
-                $text .= $page->getText();
-                $currentPage++;
-            }
+        $totalPages = count($pages);
+        $currentPage = 1;
+        $text = "";
+        foreach ($pages as $page) {
+            $text .= "<h3>Page $currentPage/$totalPages</h3> </br>";
+            $text .= $page->getText();
+            $currentPage++;
+        }
         // $text = str_replace("\n", "</br>", $text);
         // $text = str_replace("\r", "</br>", $text);
         $text = str_replace("\t", "", $text);
