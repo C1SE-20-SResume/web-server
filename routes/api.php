d7a29dfa4ea1f4e09ea181d8db6776f8c871e3dc
Thanh Ngoc <nguyenngocthanh12@dtu.edu.vn>
2021-11-07 08:23:35 +0000

    update route

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\JobApplyController;
 use App\Http\Controllers\API\JobDetailController;
 
