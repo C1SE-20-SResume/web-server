0017fe008e606cef66b510ea402860a726ec6867
Thang Truong <thang11a5tp@gmail.com>
2021-11-08 18:38:55 +0700

    remove test scan cv

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\JobApplyController;
 use App\Http\Controllers\API\JobDetailController;
-use App\Http\Controllers\API\ScanCV;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -59,9 +59,9 @@ Route::group(['middleware' => 'auth:api'], function () {
      * @queryParam required: api_token
      */
     Route::get('logout', [UserController::class, 'logout']);
-    
+
     //---API OF RECRUITER---
-    
+
     /**
      * Show summarizes the number of applies for each job API
      * for page 'Home' of recruiter
@@ -70,62 +70,57 @@ Route::group(['middleware' => 'auth:api'], function () {
     Route::get('recruiter/apply', [JobApplyController::class, 'index']);
 
     /**
-    * Show all apply details of a specific job API 
-    * for 'Apply Details' when click a specific apply in page 'Home' of recruiter
-    * @queryParam required: api_token
-    */
+     * Show all apply details of a specific job API 
+     * for 'Apply Details' when click a specific apply in page 'Home' of recruiter
+     * @queryParam required: api_token
+     */
     Route::get('recruiter/apply/{job_id}', [JobApplyController::class, 'show']);
 
     /**
-    * Add a specific job API 
-    * For page 'Add job' of recruiter
-    * @queryParam required: api_token, company_id, job_title, job_descrip, job_benefit, job_place, salary, job_keyword[keyword, weight]
-    */
+     * Add a specific job API 
+     * For page 'Add job' of recruiter
+     * @queryParam required: api_token, company_id, job_title, job_descrip, job_benefit, job_place, salary, job_keyword[keyword, weight]
+     */
     Route::post('recruiter/job/add', [JobDetailController::class, 'store']);
 
     /**
-    * View all jobs which recruiter of a company had added API 
-    * For page 'View job' of recruiter
-    * @queryParam required: api_token
-    */
+     * View all jobs which recruiter of a company had added API 
+     * For page 'View job' of recruiter
+     * @queryParam required: api_token
+     */
     Route::get('recruiter/job/view/{company_id}', [JobDetailController::class, 'view']);
 
     /**
-    * View all details of a job which had added to edit API 
-    * For page 'Add job' when edit but not submit yet of recruiter
-    * @queryParam required: api_token
-    */
+     * View all details of a job which had added to edit API 
+     * For page 'Add job' when edit but not submit yet of recruiter
+     * @queryParam required: api_token
+     */
     Route::get('recruiter/job/edit/{job_id}', [JobDetailController::class, 'edit']);
 
     /**
-    * Update a job which had added API 
-    * For page 'Add job' when click submit edit of recruiter
-    * @queryParam required: api_token, company_id, job_title, job_descrip, job_benefit, job_place, salary, job_keyword[keyword, weight]
-    */
+     * Update a job which had added API 
+     * For page 'Add job' when click submit edit of recruiter
+     * @queryParam required: api_token, company_id, job_title, job_descrip, job_benefit, job_place, salary, job_keyword[keyword, weight]
+     */
     Route::post('recruiter/job/update/{job_id}', [JobDetailController::class, 'update']);
 
     /**
-    * Delete a job  API 
-    * For page 'View job' of recruiter
-    * @queryParam required: api_token
-    */
+     * Delete a job  API 
+     * For page 'View job' of recruiter
+     * @queryParam required: api_token
+     */
     Route::get('recruiter/job/delete/{job_id}', [JobDetailController::class, 'destroy']);
 
 
     //---API OF CANDIDATE---
-    
 
-    
+
+
 
     /**
-    * Upload a specific CV file (.pdf) and return result API 
-    * For page 'Job details' of candidate
-    * @queryParam required: api_token, job_id, cv_file
-    */
+     * Upload a specific CV file (.pdf) and return result API 
+     * For page 'Job details' of candidate
+     * @queryParam required: api_token, job_id, cv_file
+     */
     Route::post('candidate/job/upload', [JobApplyController::class, 'store']);
 });
-
-
-// Test Scan CV file upload
-Route::post('scan', [ScanCV::class, 'index']);
-
