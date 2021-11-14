0a029aba573608957091807ce9a877915d1ed0ea
Thang Truong <thang11a5tp@gmail.com>
2021-11-14 10:49:15 +0700

    update email template

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,21 +1,6 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Verify Your Email Address</div>
-                <div class="card-body">
-                    @if (session('resent'))
-                    <div class="alert alert-success" role="alert">
-                        {{ __('A fresh verification link has been sent to your email address.') }}
-                    </div>
-                    @endif
-                    <a href="{{ url('/reset-password/'.$token) }}">Click Here</a>
-                </div>
-            </div>
-        </div>
-    </div>
+<div>
+    <h1>Reset Password</h1>
+    <p>You have requested to reset your password. Please click the link below to reset your password.</p>
+    <p><a href="{{ url('/reset-password/'.$token) }}">Reset Your Password</a></p>
+    <p>If you did not request a password reset, please ignore this email.</p>
 </div>
-@endsection
