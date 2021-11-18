b8006b5b207477e3d2c88db96a24879ecbf024d5
Thang Truong <thang11a5tp@gmail.com>
2021-11-18 16:42:27 +0700

    dev

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,13 +9,13 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <link rel="stylesheet" href="{{ asset(mix('css/app.css')) }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 </head>
 
 <body>
     <div id="app"></div>
-    <script src="{{ asset(mix('js/app.js')) }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 
 </body>
 
