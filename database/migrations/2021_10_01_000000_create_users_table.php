a069972a2ff0c62894d1cd13178e1a35e2089206
thanh12-dtu-test <62973821+thanh12-dtu-test@users.noreply.github.com>
2021-12-07 17:00:04 +0700

    database: update users table, delete admin table

--- database/migrations/2021_10_01_000000_create_users_table.php
+++ database/migrations/2021_10_01_000000_create_users_table.php
@@ -21,9 +21,11 @@ class CreateUsersTable extends Migration
             $table->date('date_birth');
             $table->string('phone_number', 10);
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable()->default(null);
             $table->string('password');
             $table->tinyInteger('role_level')->default(0);
             $table->string('api_token', 80)->unique()->nullable()->default(null);
+            // $table->rememberToken();
             $table->timestamps();
         });
     }
